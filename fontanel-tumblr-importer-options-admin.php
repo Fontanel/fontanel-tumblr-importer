54f06358f9258624345ff89b4fe5602804bb6680
Jasper Kennis <jasper@fontanel.nl>
2013-04-11 11:34:43 +0200

    Created a table with all posted articles from notes and a link to delete one.

--- fontanel-tumblr-importer-options-admin.php
+++ fontanel-tumblr-importer-options-admin.php
@@ -5,4 +5,35 @@
 		<?php do_settings_sections( 'fontanel-tumblr-importer-options' ); ?>
 		<?php submit_button(); ?>
 	</form>
+	
+	<style>
+  	table { border-collapse: collapse; }
+    table, tr, td {
+       border: 1px solid black;
+    }
+	</style>
+	
+	<script>
+  	function confirmDelete(delUrl) {
+      if (confirm("Weet je het zeker? Als je per ongeluk de verkeerde post verwijderd word Jasper boos!")) {
+        document.location = delUrl;
+      }
+    }
+	</script>
+	
+	<table>
+  	<tbody>
+      <?php $MyFontanelTumblrImporter = new FontanelTumblrImporter(); ?>
+      <?php foreach( $MyFontanelTumblrImporter->getPosts( $_GET['page'], 5,  true ) as $tumblr_post ): ?>
+        <tr>
+          <td>
+            <?php echo $MyFontanelTumblrImporter->defaultPostDisplay( $tumblr_post ); ?>
+          </td>
+          <td style="vertical-align: top;">
+            <a href="javascript:confirmDelete('delete.page?id=1')">Verwijder dit artikel van de Magazine Homepage</a>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+	</table>
 </div>
